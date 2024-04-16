6b34e583c8b159fb7c2c81e27f1f2eaf7da4ac9e
alpha-abhii <alpha20abhi@gmail.com>
2024-04-17 01:46:55 +0530

    ui library setup

--- 02_pizza/src/app/page.tsx
+++ 02_pizza/src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-center text-4xl text-green-300">Hello Abhishek</h1>
+    <main>
+      <h1 className="text-center text-4xl text-green-300">Hello Abhishek</h1>
+      <Button variant="destructive">Button</Button>
+    </main>
   );
 }
