d4ffa057d63856d6b3ed39b569f9444eb3ba3806
alpha-abhii <alpha20abhi@gmail.com>
2024-05-29 14:26:55 +0530

    add logo and select box and assets

--- 02_pizza/src/app/(home)/page.tsx
+++ 02_pizza/src/app/(home)/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <h1 className="text-center text-4xl text-green-300">Hello Abhishek</h1>
       <Button>Button</Button>
-    </main>
+    </>
   );
 }
