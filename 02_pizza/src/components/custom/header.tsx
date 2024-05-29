4362491198b15b9f3b9696b8fd695f45be7f204e
alpha-abhii <alpha20abhi@gmail.com>
2024-05-29 15:10:13 +0530

    add navbar

--- 02_pizza/src/components/custom/header.tsx
+++ 02_pizza/src/components/custom/header.tsx
@@ -6,12 +6,15 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select"
+import Link from 'next/link'
+import { Phone, ShoppingBasket } from 'lucide-react'
+import { Button } from '../ui/button'
 
 
 const Header = () => {
     return (
         <header className='bg-white'>
-            <nav className='container py-5 '>
+            <nav className='container py-5 flex justify-between'>
                 <div className='flex items-center space-x-4'>
                     <svg
                         data-testid="logo"
@@ -36,7 +39,29 @@ const Header = () => {
                             <SelectItem value="kids-corner">Kids Corner</SelectItem>
                         </SelectContent>
                     </Select>
-
+                </div>
+                <div className='flex items-center gap-x-4'>
+                    <ul className='flex items-center font-medium space-x-4'>
+                        <li>
+                            <Link className='hover:text-primary' href={"/"}>Menu</Link>
+                        </li>
+                        <li>
+                            <Link className='hover:text-primary' href={"/"}>Orders</Link>
+                        </li>
+                    </ul>
+                    <div className='relative'>
+                        <Link className='hover:text-primary' href={"/cart"}>
+                            <ShoppingBasket />
+                        </Link>
+                        <span className='absolute -top-4 -right-5 h-6 w-6 flex items-center justify-center rounded-full bg-primary font-bold text-white'>
+                            3
+                        </span>
+                    </div>
+                    <div className='flex items-center gap-x-2 ml-12'>
+                        <Phone/>
+                        <span>+91 9800 098 998</span>
+                    </div>
+                    <Button size={'sm'}>Logout</Button>
                 </div>
             </nav>
         </header>
